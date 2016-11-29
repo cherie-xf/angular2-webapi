141af62b1fa3824e763a03dd583ffe61fcbff295
Cherie Fu <cheriefu@fortinet.com>
2016-11-28 18:23:03 -0800

    add change week

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -14,6 +14,8 @@ export class EventComponent implements OnInit {
   ctrlFlag = '';
   public rows:Array<any> = [];
   activities: Activity[] = [];
+  events: Array<any> = [];
+  currentWeek: Array<Date> = [];
   public columns:Array<any> = [
 	  {title: 'From Date', name: 'fromDate'},
 	  {title: 'To Date', name: 'toDate'},
@@ -29,32 +31,52 @@ export class EventComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.currentWeek = this.getCurrentWeek(new Date());
     this.onChangeTable();
   }
 
   public onChangeTable() {
+      this.events = [];
 	  this.activityService.getAll().subscribe(activities => { this.activities = activities; });
       this.eventService.getAll().subscribe(events => { this.rows = events;
         for (var i = 0; i < this.rows.length; i++) {
-          var row = this.rows[i];
-          var fromF = row.fromDate.split('T')[0];
-          var fromB = row.fromDate.split('T')[1];
+          let row = this.rows[i];
+          let fromF = row.fromDate.split('T')[0];
+          let fromB = row.fromDate.split('T')[1];
           row.fromY = parseInt(fromF.split('-')[0]);
           row.fromM = parseInt(fromF.split('-')[1])-1;
           row.fromD = parseInt(fromF.split('-')[2]);
           row.fromh = parseInt(fromB.split(':')[0]);
           row.fromm = parseInt(fromB.split(':')[1]);
           row.froms = parseInt(fromB.split(':')[2]);
-          var toF = row.toDate.split('T')[0];
-          var toB = row.toDate.split('T')[1];
+          let toF = row.toDate.split('T')[0];
+          let toB = row.toDate.split('T')[1];
           row.toY = parseInt(toF.split('-')[0]);
           row.toM = parseInt(toF.split('-')[1])-1;
           row.toD = parseInt(toF.split('-')[2]);
           row.toh = parseInt(toB.split(':')[0]);
           row.tom = parseInt(toB.split(':')[1]);
           row.tos = parseInt(toB.split(':')[2]);
+          row.fromRealDate = new Date(row.fromY, row.fromM, row.fromD, row.fromh, row.fromm, row.froms);
+          row.toRealDate = new Date(row.toY, row.toM, row.toD, row.toh, row.tom, row.tos);
+
+          // push current data to events
+            if(this.currentWeek[0] && this.currentWeek[1]){
+                if((row.fromRealDate.getTime()>= this.currentWeek[0].getTime() &&
+                   row.fromRealDate.getTime()<= this.currentWeek[1].getTime()) ||
+                   (row.toRealDate.getTime()>= this.currentWeek[0].getTime() &&
+                   row.toRealDate.getTime()<= this.currentWeek[1].getTime()) ){
+                    this.events.push(row);
+                 }
+            }
         }
+
       });
+  }
+
+  public edit(id) {
+    this.ctrlFlag = 'edit';
+    this.eventService.getById(id).subscribe(event => { this.model = event });
   }
 
   public delete(id){
@@ -108,6 +130,47 @@ export class EventComponent implements OnInit {
   public cancel(){
 	  this.model = {};
     this.ctrlFlag = '';
+  }
+
+  /*
+  private getCurrentWeek(date: Date) {
+      let now = date? new Date(date) : new Date(); 
+      // set time to some convenient value 
+      now.setHours(0,0,0,0); 
+      // Get the previous Monday 
+      let monday = new Date(now); 
+      monday.setDate(monday.getDate() - monday.getDay() + 1); 
+      // Get next Sunday 
+      let sunday = new Date(now); 
+      sunday.setDate(sunday.getDate() - sunday.getDay() + 7); 
+      // Return array of date objects 
+      return [monday, sunday];
+  }
+  */
+  private getCurrentWeek(date: Date) {
+
+  let curr = date ||new Date; // get current date
+  curr.setHours(0,0,0,0); 
+  let first = curr.getDate() - curr.getDay() + 1; // First day is the day of the month - the day of the week
+  let last = first + 7; // last day is the first day + 6
+  var firstday = new Date(curr.setDate(first));
+  var lastday = new Date(curr.setDate(last));
+  return [firstday, lastday];
+
+  }
+  public preweek() {
+    let newDate = new Date(this.currentWeek[0].getTime() - 86400000 * 7);
+    this.currentWeek = this.getCurrentWeek(newDate);
+    this.onChangeTable();
+  }
+  public curweek() {
+    this.currentWeek = this.getCurrentWeek(new Date());
+    this.onChangeTable();
+  }
+  public nextweek() {
+    let newDate = new Date(this.currentWeek[0].getTime() + 86400000 * 7);
+    this.currentWeek = this.getCurrentWeek(newDate);
+    this.onChangeTable();
   }
 
 }
