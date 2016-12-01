2a75f86f6574dd0c9037d7c1b8e2877306ee313a
cherie <cherie.x.fu@gmail.com>
2016-12-01 00:29:23 -0800

    1201 rebuild dist

--- dist/styles.b2328beb0372c051d06d.bundle.js
+++ dist/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{430:function(n,o){},985:function(n,o,c){n.exports=c(430)}},[985]);
+webpackJsonp([1,2],{432:function(n,o){},988:function(n,o,c){n.exports=c(432)}},[988]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
